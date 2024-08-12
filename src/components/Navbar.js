d30b6273d3b99d0c940112601bf35e1d5307c810
white <goat@yyds.com>
2024-08-12 18:37:54 +0800

    Your commit message

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react"
+export default function Main () {
+    return (
+        <nav>
+            <img src="/logo192.png" alt="Logo " className="nav--icon"/>
+            <h3 className="nav--logo-text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
+        </nav>
+    )
+    
+}
